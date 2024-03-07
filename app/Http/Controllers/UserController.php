5c4f11bf3d4267a930b879acceb27db951c3ac6e
Sila Pakdeewong <silapakdeewong2546.3@gmail.com>
2024-03-07 22:44:55 +0700

    add: UserController store() for create new user

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,7 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Credentials;
+use App\Models\AcademicInfo;
 use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
+use Illuminate\Foundation\Auth\User;
+use Illuminate\Support\Facades\Validator;
 
 class UserController extends Controller
 {
@@ -27,7 +32,45 @@ class UserController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        Validator::make(
+            $request->all(),
+            [
+                'role' => ['required', Rule::in(['LEARNER', 'LECTURER'])],
+                'email' => ['required', 'email', Rule::unique('users', 'email')],
+                'username' => ['required', Rule::unique('credentials', 'username')],
+                'password' => ['required', 'min:8', 'confirmed'],
+            ],
+            [
+                'role.required' => 'โปรดเลือกบทบาท (Role)',
+                'role.in' => 'โปรดเลือกบทบาท (Role) ที่ถูกต้อง',
+                'username.required' => 'โปรดกรอกชื่อผู้ใช้',
+                'username.unique' => 'ชื่อผู้ใช้นี้ถูกใช้ไปแล้ว',
+                'password.required' => 'โปรดกรอกรหัสผ่าน',
+                'password.min' => 'รหัสผ่านต้องมีอย่างน้อย 8 ตัวอักษร',
+                'password.confirmed' => 'รหัสผ่านไม่ตรงกัน',
+                'email.required' => 'โปรดกรอกอีเมล',
+                'email.email' => 'โปรดกรอกอีเมลที่ถูกต้อง',
+                'email.unique' => 'อีเมลนี้ถูกใช้ไปแล้ว',
+            ]
+        );
+
+        $academicInfo = AcademicInfo::create();
+
+        $credentials = Credentials::create([
+            'username' => $request->input('username'),
+            'password' => bcrypt($request->input('password')),
+        ]);
+
+        $user = User::create([
+            'role' => $request->input('role'),
+            'email' => $request->input('email'),
+            'credential_id' => $credentials->id,
+            'academic_id' => $academicInfo->id,
+        ]);
+
+        auth()->login($user);
+
+        return redirect('/')->with('success_message', 'สร้างบัญชีผู้ใช้งานสำหรับ ' . $request->input('username') . ' เสร็จสิ้น');
     }
 
     /**
