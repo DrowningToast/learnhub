523cc2a08d6ebbda0f7c52ea923a85892b9bbc94
DrowningToast <supratouchgussii@gmail.com>
2024-03-08 16:22:34 +0700

    add: banner

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/courses/{id}', function ($id) {
+    return view('course_info');
+});
