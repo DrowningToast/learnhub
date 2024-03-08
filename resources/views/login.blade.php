3ed5735c2cb46060b68f304f85e7481c2c8575b1
Sila Pakdeewong <silapakdeewong2546.3@gmail.com>
2024-03-08 17:47:46 +0700

    feat: add /login page & icons

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,35 +1,96 @@
-<x-layout>
-    <div class="mx-auto flex items-center justify-center">
-        <div class=" p-12 border rounded-xl flex flex-col items-start justify-start w-1/3">
-            <h1 class="text-3xl font-bold mb-6">เข้าสู่ระบบ</h1>
+<!DOCTYPE html>
+<html lang="en">
 
-            <div class="w-full">
-                <form action="/login" method="POST">
-                    @csrf
-                    <div class="my-4">
-                        <label for="email" class="block text-sm font-semibold mb-2">อีเมล</label>
-                        <input type="email" name="email" id="email"
-                            class="w-full border border-gray-300 p-2 rounded" value="{{ old('email') }}">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>เข้าสู่ระบบ | LearnHub</title>
 
-                        @error('email')
-                            <p class="text-red-500 text-xs mt-2">{{ $message }}</p>
-                        @enderror
+    @vite('resources/css/app.css')
+</head>
+
+<body class="p-6 min-h-screen font-noto-thai">
+    <div class="grid grid-cols-2 h-full">
+        {{-- RIGHT SIDE --}}
+        <div>
+            <div class="flex flex-col justify-center min-h-full lg:px-28 2xl:px-36">
+                <div>
+                    <p class=" text-3xl font-bold mb-4">เข้าสู่ระบบ</p>
+                    <p>ยังไม่มีบัญชีผู้ใช้ ? <span class="text-[#4C5F7C]"><a href="/register">สมัครสมาชิก</a></span></p>
+                </div>
+
+                <div class="mt-16 flex flex-col gap-12">
+                    <div class="flex flex-col gap-2">
+                        <label for="username" class="text-[#999999] font-bold">ชื่อผู้ใช้</label>
+                        <div class="flex flex-row items-center gap-2 relative">
+                            <img src="{{ asset('images/icons/message.png') }}" class=" absolute">
+                            <input type="text" id="username"
+                                class="w-full p-2 border-b-2 border-b-[#999999]  focus:outline-none focus:border-b-[#000842] pl-8"
+                                placeholder="ใส่ชื่อผู้ใช้ของคุณ" />
+                        </div>
                     </div>
 
-                    <div class="my-4">
-                        <label for="password" class="block text-sm font-semibold mb-2">รหัสผ่าน</label>
-                        <input type="password" name="password" id="password"
-                            class="w-full border border-gray-300 p-2 rounded">
+                    <div class="flex flex-col gap-2">
+                        <label for="username" class="text-[#999999] font-bold">รหัสผ่าน</label>
+                        <div class="flex flex-row items-center gap-2 relative">
+                            <img src="{{ asset('images/icons/lock.png') }}" class=" absolute">
+                            <input type="password" id="username"
+                                class="w-full p-2 border-b-2 border-b-[#999999]  focus:outline-none focus:border-b-[#000842] pl-8"
+                                placeholder="ใส่รหัสผ่านของคุณ" />
+                        </div>
+                    </div>
+                </div>
 
-                        @error('password')
-                            <p class="text-red-500 text-xs mt-2">{{ $message }}</p>
-                        @enderror
+                <div class=" mt-8 flex flex-row justify-between">
+                    <div class="flex flex-row items-center gap-2">
+                        <input type="checkbox" id="saveSession" name="saveSession" value="saveSession" class="h-5 w-5">
+                        <label for="saveSession" class="text-[#000000]"> จดจำการเข้าสู่ระบบ</label><br>
                     </div>
 
-                    <button type="submit" class="w-full text-black p-2 rounded mt-6 bg-blue-300">เข้าสู่ระบบ</button>
-                    <a href="/register" class="text-center mt-6 w-full flex justify-center underline">สมัครสมาชิก</a>
-                </form>
+                    <div>
+                        <p class="text-[#4D4D4D] font-light">
+                            <a href="">
+                                ลืมรหัสผ่าน ?
+                            </a>
+                        </p>
+                    </div>
+                </div>
+
+                <div class="mt-12">
+                    <button
+                        class="w-full bg-[#2A638A] text-white rounded-3xl font-bold focus:outline-none py-5 text-lg">เข้าสู่ระบบ</button>
+                </div>
+
+                <div class="mt-12 flex flex-col items-center">
+                    <p class="text-[#B5B5B5] font-medium">หรือดำเนินการต่อด้วย</p>
+
+                    <div class="flex flex-row items-center gap-4 mt-6">
+                        <img src="{{ asset('images/icons/Facebook.png') }}" class="">
+                        <img src="{{ asset('images/icons/apple.png') }}" class="">
+                        <img src="{{ asset('images/icons/google.png') }}" class="">
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        {{-- LEFT SIDE --}}
+        <div class="min-w-full min-h-full bg-gradient-to-b from-[#D4E3FF] to-[#B4C8E9] rounded-xl">
+            <div class="flex flex-col items-start justify-start">
+                <div class="flex items-center justify-center">
+                    <img src="{{ asset('images/vector/Saly-10.png') }}" class="w-[80%]" />
+                </div>
+
+
+                <div class="p-24 pt-18">
+                    <p class=" font-poppins text-4xl text-white">เข้าสู่ระบบ
+                        <span class="font-beth text-[#2560B4]">LearnHub</span>
+                    </p>
+                    <p class="font-poppins mt-3 text-lg text-white">Lorem Ipsum is simply</p>
+                </div>
             </div>
         </div>
     </div>
-</x-layout>
+</body>
+
+</html>
