b789e3f13f970e977b2762e7ff426d4932ecb206
DrowningToast <supratouchgussii@gmail.com>
2024-03-10 02:48:19 +0700

    add: seed users

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,10 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Enums\RoleEnum;
+use App\Models\Credentials;
+use App\Models\Users;
+use Database\Factories\UsersFactory;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,11 +16,45 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        $learnerCred = Credentials::factory()->create([
+            "username" => "learner",
+            "password" => bcrypt("password"),
+        ]);
+
+        $learner = Users::factory()->create([
+            "credential_id" => $learnerCred->id,
+            "role" => RoleEnum::Learner,
+        ]);
+
+        $bots = Users::factory(10)->create();
+
+        $lecturerCred = Credentials::factory()->create([
+            "username" => "lecturerA",
+            "password" => bcrypt("password"),
+        ]);
+
+        $lecturer = Users::factory()->create([
+            "credential_id" => $lecturerCred->id,
+            "role" => RoleEnum::Lecturer,
+        ]);
+
+        $moderatorCred = Credentials::factory()->create([
+            "username" => "moderatora",
+            "password" => bcrypt("password"),
+        ]);
+
+        $admin = Users::factory()->create([
+            "credential_id" => $moderatorCred->id,
+            "role" => RoleEnum::Moderator,
+        ]);
+
         // \App\Models\User::factory(10)->create();
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        // User::create
     }
 }
