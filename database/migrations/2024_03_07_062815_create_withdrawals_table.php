417275be05cbc0cb41be874d25852630a53d31bd
Sila Pakdeewong <silapakdeewong2546.3@gmail.com>
2024-03-10 23:31:48 +0700

    delete unused comments

--- database/migrations/2024_03_07_062815_create_withdrawals_table.php
+++ database/migrations/2024_03_07_062815_create_withdrawals_table.php
@@ -13,12 +13,6 @@ return new class extends Migration {
         Schema::create('withdrawals', function (Blueprint $table) {
             $table->id();
 
-            // $table->unsignedBigInteger("user_id")->unique()->nullable();
-            // $table->foreign("user_id")->references("id")->on("users")->onDelete("cascade");
-
-            // $table->unsignedBigInteger("bank_id")->unique()->nullable();
-            // $table->foreign("bank_id")->references("id")->on("banks")->onDelete("cascade");
-
             $table->foreignId("user_id")->constrained('users', 'id')->onDelete("cascade");
 
             $table->float("amount");
