b4235be93b65e8e99a3eb507f40455e1f9f257cc
DrowningToast <supratouchgussii@gmail.com>
2024-03-10 14:43:20 +0700

    fix: chapter unique course id

--- database/migrations/2024_03_07_075816_create_chapters_table.php
+++ database/migrations/2024_03_07_075816_create_chapters_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('chapters', function (Blueprint $table) {
             $table->id();
 
-            $table->unsignedBigInteger("course_id")->unique();
+            $table->unsignedBigInteger("course_id");
             $table->foreign("course_id")->references("id")->on("courses")->onDelete("cascade");
 
             $table->string("title");
