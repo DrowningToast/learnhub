cd81cb352ceb5becf9b0bb78a99cc21ea7d7a688
SarutaBamboo <65070211@kmitl.ac.th>
2024-03-12 18:02:46 +0700

    refact: change name

---
+++ app/View/Components/StarRating.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\View\Components;
+
+use Closure;
+use Illuminate\View\Component;
+use Illuminate\Contracts\View\View;
+
+class StarRating extends Component
+{
+    /**
+     * Create a new component instance.
+     */
+    public function __construct(
+        public float $rating = 0.0
+    ) {
+        //
+    }
+
+    /**
+     * Get the view / contents that represent the component.
+     */
+    public function render(): View|Closure|string
+    {
+        return view('components.star-rating');
+    }
+}
