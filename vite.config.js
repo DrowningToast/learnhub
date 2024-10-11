a5dac51de3dc7d6bfe9baa490c3b1ad2b59330b8
FewPz <sharpkung4826@gmail.com>
2024-10-11 13:33:09 +0700

    refactor: update vite.config.js to enhance HMR configuration with server IP and WebSocket protocol

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,12 @@ export default defineConfig({
         }),
     ],
     server: {
-        host: '0.0.0.0', // Allow external access
+        host: '0.0.0.0', // Allow external access from any network
         port: 5173,
-    }
+        hmr: {
+            host: '34.143.197.151', // Your server's public IP address
+            port: 5173,
+            protocol: 'ws', // WebSocket protocol for HMR
+        },
+    },
 });
